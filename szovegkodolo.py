2b73a82f471816d7350f11cb2b5bdcf1f3bd5391
István Gy. Zsély <zselyigy@gmail.com>
2023-12-11 22:09:04 +0100

    extended to capital letters and kepp any other symbol

--- szovegkodolo.py
+++ szovegkodolo.py
@@ -1,14 +1,17 @@
 alphabet = 'aábcdeéfghiíjklmnoóöőpqrstuúüűvwxyz'
+capitalalphabet = alphabet.upper()
 la = len(alphabet)
 
 a = input('Írd be az átalakítandó szöveget! ')
 
 b = ''
 for i in range(len(a)):
-    if a[i] == ' ':
-        mychr = ' '
-    else:
+    if a[i] in alphabet:
         mychr = alphabet[la - alphabet.find(a[i])-1]
+    elif a[i] in capitalalphabet:
+        mychr = capitalalphabet[la - capitalalphabet.find(a[i])-1]
+    else:
+        mychr = a[i]
     b = b + mychr
 
 print(b)
